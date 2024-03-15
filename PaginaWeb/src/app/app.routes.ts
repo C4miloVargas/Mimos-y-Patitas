edd0c1ad913403eb5fe7e22a649b3fb4a26e18e7
Camilo Vargas <camilo_vargasam@fet.edu.co>
2024-03-15 07:34:04 -0500

    Segundo commit
    Cambio de pestaña inicial

--- PaginaWeb/src/app/app.routes.ts
+++ PaginaWeb/src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { FormComponent } from './pages/form/form.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
 
 export const routes: Routes = [
-    { path: '', component: PerfilComponent},
+    { path: '', component: InicioComponent},
     { path: 'inicio', component: InicioComponent },
     { path: 'login', component: LoginComponent },
     { path: 'formulario', component: FormComponent },
